956e62dec58b47c4ccb850e055e8ea1db049662d
Taro Omiya <taro.omiya@omiyagames.com>
2015-04-15 21:41:24 +0000

    Step 10: Adding audio, creating a prefab.

---
+++ Assets/PlaySoundOnCollision.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+public class PlaySoundOnCollision : MonoBehaviour {
+	AudioSource audio;
+	void Start () {
+		audio = GetComponent<AudioSource>();
+	}
+	void OnCollisionEnter(Collision info) {
+		audio.Stop();
+		audio.Play();
+	}
+}
