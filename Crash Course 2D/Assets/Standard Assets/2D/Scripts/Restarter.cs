df593f536ec46580b732fc711ceaa1cccb0dad7d
Taro Omiya <taro.omiya@omiyagames.com>
2016-04-12 00:38:22 +0000

    Reverting Restarter.cs to its original, broken state.

--- Crash Course 2D/Assets/Standard Assets/2D/Scripts/Restarter.cs
+++ Crash Course 2D/Assets/Standard Assets/2D/Scripts/Restarter.cs
@@ -10,8 +10,7 @@ namespace UnityStandardAssets._2D
         {
             if (other.tag == "Player")
             {
-                Debug.Log(SceneManager.GetSceneAt(0).path);
-                SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+                SceneManager.LoadScene(SceneManager.GetSceneAt(0).path);
             }
         }
     }
